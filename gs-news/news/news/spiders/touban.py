436da1b594ff8dca9a097d1ebe36eb68a4ae17b7
x_false <514286291@qq.com>
2021-08-19 16:55:21 +0800

    ✨ feat:
    完善

--- gs-news/news/news/spiders/touban.py
+++ gs-news/news/news/spiders/touban.py
@@ -4,22 +4,52 @@ import scrapy
 class ToubanSpider(scrapy.Spider):
     name = 'touban'
     start_urls = ['https://book.douban.com/tag/?view=type']
+    global base_url
+    base_url = 'https://book.douban.com'
 
     def parse(self, response):
         for item in response.xpath('//*[@id="content"]/div/div[1]/div[2]'):
-
-            tagName = item.xpath(
+            tagNames = item.xpath(
                 '//div/table[@class="tagCol"]//tr//a//text()').extract()
-            # tagUrl = item.xpath(
-            #     '//div/table[@class="tagCol"]//tr//a/@href').extract()
+            tagUrls = item.xpath(
+                '//div/table[@class="tagCol"]//tr//a/@href').extract()
 
-            titles = item.xpath('//div/a/h2//text()').getall()
-            tagTitle = []
-            for title in titles:
-                tagTitle.append(title.replace(' · · · · · · ', ''))
+            for tagUrl in tagUrls:
+                yield scrapy.Request(base_url + tagUrl, callback=self.parse_book_list)
 
-            # yield scrapy.Request(url, callback=self.parse_content)
+    def parse_book_list(self, response):
+        for list_item in response.css('div#content div#subject_list ul.subject-list li.subject-item'):
 
-            yield {
-                "tagTitle": tagTitle, "tagName": tagName
+            bookSummary = list_item.css('div.info p::text').extract()
+
+            bookUrl = list_item.css('div.info h2 a::attr(href)').extract()
+
+            for name in list_item.css('div.info h2 a::text').extract():
+                if(name):
+                    bookName = name.strip()
+
+            for pub in list_item.css('div.info div.pub::text').extract():
+                if(pub):
+                    bookPub = pub.strip()
+
+            for rating in list_item.css('div.info div.star span.rating_nums::text').extract():
+                if(rating):
+                    bookRatingNums = rating.strip()
+
+            for pl in list_item.css('div.info div.star span.pl::text').extract():
+                if(pl):
+                    bookPl = pl.strip()
+            yield{
+                'bookName': bookName,
+                'bookPub': bookPub,
+                'bookRatingNums': bookRatingNums,
+                'bookPl': bookPl,
+                'bookSummary': bookSummary,
+                'bookUrl': bookUrl,
             }
+
+        next_page = response.css('div.paginator span.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            print('=>'*10, next_page)
+            yield scrapy.Request(base_url + next_page, callback=self.parse_book_list)
